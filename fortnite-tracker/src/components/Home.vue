944559aab212654a3bf800537c3d21026f6c68d9
Jason <jason@astucemedia.com>
2018-01-21 10:56:32 -0500

    minor changes

--- fortnite-tracker/src/components/Home.vue
+++ fortnite-tracker/src/components/Home.vue
@@ -3,105 +3,115 @@
 
     <div v-if="loading">
 
-      <div v-if="error" class="alert">
-        <strong>Error!</strong> There was an error trying to get your stats. Please wait 5 seconds and try again. If you don't wait 5 seconds - I will find you...
+      <div class="alert alert-danger mt-3" role="alert" v-if="error">
+        <strong>Error!</strong> There was an error trying to get your stats. Please wait 20 seconds and try again. If you don't wait 20 seconds - I will find you...
       </div>
 
-      <Loader />
-      <div class="text-center mt-3">You are never too old to set another goal or to dream a new dream <em>- C.S Lewis</em></div>
+      <Loader v-if="!error"/>
     </div>
 
-    <div class="p-3 fade-in" v-if="!loading">
-      <div class="d-flex justify-between">
-        <h3>Lifetime</h3>
-        <button class="button-primary" @click="refresh()">Refresh</button>
-      </div>
-      <table class="u-full-width">
-        <thead>
-            <tr>
-              <th>User</th>
-              <th>Wins</th>
-              <th>Win %</th>
-              <th>Top 3</th>
-              <th>Top 5</th>
-              <th>Top 6</th>
-              <th>Top 12</th>
-              <th>Top 25</th>
-              <th>Kills</th>
-              <th>KD</th>
-              <th>Kills/Min</th>
-              <th>Score</th>
-              <th>Matches Played</th>
-              <th>Avg. Time Played</th>
-            </tr>
-        </thead>
-        <tbody>
-          <tr v-for="d in data" :key="d.accountId">
-            <td>{{d.epicUserHandle}}</td>
-            <td>{{d.lifeTimeStatsMap['Wins']}}</td>
-            <td>{{d.lifeTimeStatsMap['Win%']}}</td>
-            <td>{{d.lifeTimeStatsMap['Top 3s']}}</td>
-            <td>{{d.lifeTimeStatsMap['Top 5s']}}</td>
-            <td>{{d.lifeTimeStatsMap['Top 6s']}}</td>
-            <td>{{d.lifeTimeStatsMap['Top 12s']}}</td>
-            <td>{{d.lifeTimeStatsMap['Top 25s']}}</td>
-            <td>{{d.lifeTimeStatsMap['Kills']}}</td>
-            <td>{{d.lifeTimeStatsMap['K/d']}}</td>
-            <td>{{d.lifeTimeStatsMap['Kills Per Min']}}</td>
-            <td>{{d.lifeTimeStatsMap['Score']}}</td>
-            <td>{{d.lifeTimeStatsMap['Matches Played']}}</td>
-            <td>{{d.lifeTimeStatsMap['Avg Survival Time']}}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="fade-in mt-3" v-if="!loading">
 
-      <div v-for="mode in modes" :key="mode.code">
-        <h3>{{mode.name}}</h3>
-        <table class="u-full-width">
-          <thead>
-            <tr>
-              <th>User</th>
-              <th>Wins</th>
-              <th>Win %</th>
-              <th>Top 3</th>
-              <th>Top 5</th>
-              <th>Top 6</th>
-              <th>Top 10</th>
-              <th>Top 12</th>
-              <th>Top 25</th>
-              <th>Kills</th>
-              <th>KD</th>
-              <th>Kills/Min</th>
-              <th>Kills/Game</th>
-              <th>Score/Min</th>
-              <th>Score/Game</th>
-              <th>Avg. Time Played</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="d in data" :key="d.accountId" v-if="d.stats[mode.code]">
-              <td>{{d.epicUserHandle}}</td>
-              <td>{{d.stats[mode.code].top1 ? d.stats[mode.code].top1.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].winRatio ? d.stats[mode.code].winRatio.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].top3 ? d.stats[mode.code].top3.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].top5 ? d.stats[mode.code].top5.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].top6 ? d.stats[mode.code].top6.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].top10 ? d.stats[mode.code].top10.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].top12 ? d.stats[mode.code].top12.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].top25 ? d.stats[mode.code].top25.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].kills ? d.stats[mode.code].kills.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].kd ? d.stats[mode.code].kd.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].kpm ? d.stats[mode.code].kpm.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].kpg ? d.stats[mode.code].kpg.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].scorePerMin ? d.stats[mode.code].scorePerMin.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].scorePerMatch ? d.stats[mode.code].scorePerMatch.displayValue : '-'}}</td>
-              <td>{{d.stats[mode.code].avgTimePlayed ? d.stats[mode.code].avgTimePlayed.displayValue : '-'}}</td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="container-fluid">
+        <div class="d-flex justify-content-between">
+          <h3>Lifetime</h3>
+          <button class="btn btn-primary" @click="refresh()">Refresh</button>
+        </div>
+
+        <div v-fixedFirstCol class="table-responsive mt-3">
+          <table class="table table-sm">
+            <thead>
+                <tr>
+                  <th class="fixed-col">User</th>
+                  <th>Wins</th>
+                  <th>Win %</th>
+                  <th>Top 3</th>
+                  <th>Top 5</th>
+                  <th>Top 6</th>
+                  <th>Top 12</th>
+                  <th>Top 25</th>
+                  <th>Kills</th>
+                  <th>KD</th>
+                  <th>Kills/Min</th>
+                  <th>Score</th>
+                  <th>Matches Played</th>
+                  <th>Avg. Time Played</th>
+                </tr>
+            </thead>
+            <tbody>
+              <tr v-for="d in data" :key="d.accountId">
+                <td class="fixed-col">{{d.epicUserHandle}}</td>
+                <td>{{d.lifeTimeStatsMap['Wins']}}</td>
+                <td>{{d.lifeTimeStatsMap['Win%']}}</td>
+                <td>{{d.lifeTimeStatsMap['Top 3s']}}</td>
+                <td>{{d.lifeTimeStatsMap['Top 5s']}}</td>
+                <td>{{d.lifeTimeStatsMap['Top 6s']}}</td>
+                <td>{{d.lifeTimeStatsMap['Top 12s']}}</td>
+                <td>{{d.lifeTimeStatsMap['Top 25s']}}</td>
+                <td>{{d.lifeTimeStatsMap['Kills']}}</td>
+                <td>{{d.lifeTimeStatsMap['K/d']}}</td>
+                <td>{{d.lifeTimeStatsMap['Kills Per Min']}}</td>
+                <td>{{d.lifeTimeStatsMap['Score']}}</td>
+                <td>{{d.lifeTimeStatsMap['Matches Played']}}</td>
+                <td>{{d.lifeTimeStatsMap['Avg Survival Time']}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="mt-3" v-for="mode in modes" :key="mode.code">
+          <h3>{{mode.name}}</h3>
+          <div v-fixedFirstCol class="table-responsive mt-3">
+            <table class="table table-sm">
+              <thead>
+                <tr>
+                  <th class="fixed-col">User</th>
+                  <th>Wins</th>
+                  <th>Win %</th>
+                  <th>Top 3</th>
+                  <th>Top 5</th>
+                  <th>Top 6</th>
+                  <th>Top 10</th>
+                  <th>Top 12</th>
+                  <th>Top 25</th>
+                  <th>Kills</th>
+                  <th>KD</th>
+                  <th>Kills/Min</th>
+                  <th>Kills/Game</th>
+                  <th>Score/Min</th>
+                  <th>Score/Game</th>
+                  <th>Avg. Time Played</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="d in data" :key="d.accountId" v-if="d.stats[mode.code]">
+                  <td class="fixed-col">{{d.epicUserHandle}}</td>
+                  <td>{{d.stats[mode.code].top1 ? d.stats[mode.code].top1.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].winRatio ? d.stats[mode.code].winRatio.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].top3 ? d.stats[mode.code].top3.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].top5 ? d.stats[mode.code].top5.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].top6 ? d.stats[mode.code].top6.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].top10 ? d.stats[mode.code].top10.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].top12 ? d.stats[mode.code].top12.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].top25 ? d.stats[mode.code].top25.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].kills ? d.stats[mode.code].kills.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].kd ? d.stats[mode.code].kd.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].kpm ? d.stats[mode.code].kpm.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].kpg ? d.stats[mode.code].kpg.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].scorePerMin ? d.stats[mode.code].scorePerMin.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].scorePerMatch ? d.stats[mode.code].scorePerMatch.displayValue : '-'}}</td>
+                  <td>{{d.stats[mode.code].avgTimePlayed ? d.stats[mode.code].avgTimePlayed.displayValue : '-'}}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
 
-      Made by Jason
+      <div class="text-center p-3 mt-3 footer">
+        Made with 💖 by <a href="http://rametta.org" target="_blank">Jason</a>
+      </div>
+
     </div>
 
   </div>
@@ -172,6 +182,19 @@ export default {
       this.data = data
       this.loading = false
     }
+  },
+  directives: {
+    fixedFirstCol: {
+      inserted(el) {
+        const elems = el.getElementsByClassName('fixed-col')
+
+        el.addEventListener('scroll', (e) => {
+          for (let i = 0; i < elems.length; i++) {
+            elems[i].style.transform = `translateX(${e.target.scrollLeft}px)`
+          }
+        })
+      }
+    }
   }
 }
 </script>
@@ -180,25 +203,9 @@ export default {
 table {
   white-space: nowrap;
 }
-.d-flex {
-  display: flex;
-}
-.justify-between {
-  justify-content: space-between;
-}
-.p-3 {
-  padding: 3rem;
-}
-.mt-3 {
-  margin-top: 3rem;
-}
-.text-center {
-  text-align: center;
-}
-.alert {
-  padding: 20px;
-  background-color: #f44336;
-  color: white;
+
+.fixed-col {
+  background: white;
 }
 
 .fade-in {
@@ -212,5 +219,9 @@ table {
   to {
     opacity: 1;
   }
+}
+
+.footer {
+  background: #e8e8e8;
 }
 </style>
