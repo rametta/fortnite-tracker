0aa9fa629c53d85e938e25ae33ebad8d55bc9720
Jason <jason@astucemedia.com>
2018-03-21 15:31:27 -0400

    new video

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="mb-3 grey--text">To be played when exiting the bus...</div>
-    <iframe class="vid" src="https://www.youtube.com/embed/1lWJXDG2i0A?rel=0&amp;start=64" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <iframe class="vid" src="https://www.youtube.com/embed/ipanM9TwPeU?rel=0&amp;start=61" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     <div class="mt-3 grey--text">Made with {{getEmote}} by <a href="http://rametta.org">Jason</a></div>
   </div>
 </template>
